024f613f5abadd830c8156f94e4b3f4705c16ea1
Macuyler <macuyler@gmail.com>
2020-08-12 23:34:19 -0500

    Use logging function

--- src/output.py
+++ src/output.py
@@ -1,4 +1,17 @@
 
+def log(txt=None, err=False, blank=False, pad=False, dunn=False):
+    if pad:
+        print()
+    if blank:
+        print(txt)
+    elif err:
+        print(f' ! {txt}')
+    elif dunn:
+        print(' % All Dunn!\n')
+    else:
+        print(f' % {txt}')
+    return
+
 def show_usage(commands=[], sub=None):
     print('\n Usage: ')
     for cmd in commands:
